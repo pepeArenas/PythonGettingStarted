c42f88740baca903a49ed289a9a767298b5275ab
jesus <jesusarenasm>
2019-02-11 17:58:50 -0600

    This contains the save and read from a txt file

--- ExcerciseStudents.py
+++ ExcerciseStudents.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
@@ -18,14 +18,30 @@ def add_student(name, student_id=332):
     students.append(student)
 
 
-student_list = get_students_titlecase()
-while (True):
-    student_name = input("Enter student name: ")
-    if student_name == "N":
-        break
-    else:
-        add_student(student_name)
-        # student_id = input("Enter student ID: ")
+def save_file(student):
+    try:
+        f = open("student.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
 
-for student in students:
-    print(student)
+
+def read_file():
+    try:
+        f = open("student.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+
+read_file()
+print_student_titlcase()
+
+student_name = input("Enter student name")
+student_id = input("Enter student ID")
+
+add_student(student_name, student_id)
+save_file(student_name)
